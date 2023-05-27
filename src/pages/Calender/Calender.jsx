544657172cd7f5cc0cc3acbd0291bc1fbcf819f3
Rasel Mahmud <rasel.mahmud.dev@gmail.com>
2023-05-27 23:40:16 +0600

    fixed add new event modal && create event completed

--- src/pages/Calender/Calender.jsx
+++ src/pages/Calender/Calender.jsx
@@ -1,21 +1,66 @@
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-import { useContext, useRef, useState } from "react";
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import { useContext, useReducer, useState } from "react";
 import { AuthContext } from "../../Context/AuthProvider";
 import { useAdmin } from "../../Hooks/useAdmin";
 import EventModal from "./EventModal";
+import { useEffect } from "react";
 
 const Calender = () => {
   const { user } = useContext(AuthContext);
   const [isAdmin] = useAdmin(user?.email);
-  const [modalOpen, setModalOpen] = useState(false);
 
-  const calenderRef = useRef(null);
+  const [events, setEvents] = useState([])
+
+  const [newEvent, setNewEvent] = useReducer((state, payload)=>({
+      ...state,
+      ...payload
+  }), {
+    openModal: false,
+    startDate: new Date()
+  })
+
+
+  function handleCreateEventModal(data){ 
+    if(!data){
+      setNewEvent({
+        modalOpen: false,
+        startDate: new Date(),
+      })
+      return;
+    }
+    setNewEvent({
+      modalOpen: true,
+      startDate: data.date,
+    
+    })
+  }
+
+  useEffect(()=>{
+
+    //fetch all event from server 
+    setEvents([
+      { title: 'event', date: new Date()},
+      { title: 'event', date: new Date()},
+      { title: 'event', date: new Date()},
+    ])
+
+  }, [])
 
   const onEventAdd = (event) => {
-    let calenderApi = calenderRef.current.getApi();
-    calenderApi.add(event);
+    
+
+    // send date to server to create events 
+    setEvents((prev)=>([...prev, event]))
+    console.log(event);
+
   };
+
+  function updateHandler(event){
+
+  }
+
 
   return (
     <section className="mx-5">
@@ -30,16 +75,20 @@ const Calender = () => {
       <div style={{
         posision: "relative", zIndex: 0
       }}>
+
         <FullCalendar
-          plugins={[dayGridPlugin]}
+          plugins={[dayGridPlugin, interactionPlugin ]}
           initialView="dayGridMonth"
-          ref={calenderRef}
+          events={events}
+          dateClick={handleCreateEventModal}
+          eventClick={updateHandler}
         />
       </div>
 
       <EventModal
-        isOpen={modalOpen}
-        onClose={() => setModalOpen(false)}
+        startDate={newEvent.startDate}
+        isOpen={newEvent.modalOpen}
+        onClose={() => handleCreateEventModal()}
         onEventAdd={(event) => onEventAdd(event)}
       />
     </section>
